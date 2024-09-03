42f322885fb91fd313f5266eebfbc59f6718a87c
coder-sadhin <codersadhin@gmail.com>
2024-09-03 20:35:59 +0600

    dfs function

--- component.cpp
+++ component.cpp
@@ -3,3 +3,13 @@ using namespace std;
 const int N = 1e5 + 5;
 vector<int> v[N];
 bool vis[N];
+void dfs(int src)
+{
+    cout << src << endl;
+    vis[src] = true;
+    for (int child : v[src])
+    {
+        if (vis[child] == false)
+            dfs(child);
+    }
+}
