679d3407897a2e4002948e5ca7582ebd63fbf532
coder-sadhin <codersadhin@gmail.com>
2024-09-04 20:41:03 +0600

    valid

--- dfs_on_2d.cpp
+++ dfs_on_2d.cpp
@@ -4,3 +4,9 @@ char a[20][20];
 bool vis[20][20];
 vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 int n, m;
+bool valid(int i, int j)
+{
+    if (i < 0 || i >= n || j < 0 || j >= m)
+        return false;
+    return true;
+}
