c5178be98c388848c30d8c5fde77a13c1cff68b2
coder-sadhin <codersadhin@gmail.com>
2024-09-04 20:39:01 +0600

    valid function

--- bfs_on_2d.cpp
+++ bfs_on_2d.cpp
@@ -5,3 +5,9 @@ int dis[20][20];
 vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 int n, m;
 char a[20][20];
+bool valid(int i, int j)
+{
+    if (i < 0 || i >= n || j < 0 || j >= m)
+        return false;
+    return true;
+}
