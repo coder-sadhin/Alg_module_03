e190019883f3a1f048233af4a5b0f124eaf3e605
coder-sadhin <codersadhin@gmail.com>
2024-09-04 20:39:27 +0600

    bfs function on 2d

--- bfs_on_2d.cpp
+++ bfs_on_2d.cpp
@@ -11,3 +11,27 @@ bool valid(int i, int j)
         return false;
     return true;
 }
+void bfs(int si, int sj)
+{
+    queue<pair<int, int>> q;
+    q.push({si, sj});
+    vis[si][sj] = true;
+    dis[si][sj] = 0;
+    while (!q.empty())
+    {
+        pair<int, int> par = q.front();
+        int a = par.first, b = par.second;
+        q.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int ci = a + d[i].first;
+            int cj = b + d[i].second;
+            if (valid(ci, cj) == true && vis[ci][cj] == false)
+            {
+                q.push({ci, cj});
+                vis[ci][cj] = true;
+                dis[ci][cj] = dis[a][b] + 1;
+            }
+        }
+    }
+}
